5892edf6a3ee8b7db55f819fd248bc403d9f93f8
JennyRakete <alan.dalisdadavis@gmail.com>
2019-10-05 17:11:47 +0200

    Added sacrificial place, ceremonies, sacred texts, amount for buildings

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let followers = 0;
 let money = 0;
 let followers_per_click = 1;
 let money_per_follower = 0.01;
+let recruiting = 1;
 
 
 class Upgrade {
@@ -15,26 +16,29 @@ class Upgrade {
     this.owned = false;
   }
   buy() {
-    if(money >= this.price && !this.owned) {
+    if(money >= this.price) {
       money -= this.price;
       this.owned = true;
       this.effect();
       $("#upgrade-"+this.id+"-btn").remove();
       $("#upgrade-"+this.id+"-price").remove();
-      $("#upgrade-"+this.id).removeClass("red");
-      $("#upgrade-"+this.id).addClass("green accent-4")
+      $("#upgrade-"+this.id).removeClass("teal").addClass("green accent-4");
+    } else {
+      M.toast({html: "You need more money for this action."});
     }
   }
 }
 
 
 class Building {
-  constructor(name, description, price, effect, price_inc) {
+  constructor(id, name, description, price, effect, init, price_inc) {
+    this.id = id;
     this.name = name;
     this.description = description;
     this.price = price;
     this.price_inc = price_inc || 1.1;
     this.effect = effect;
+    this.init = init;
     this.count = 0;
   }
 
@@ -48,31 +52,47 @@ class Building {
       this.price = this.price * this.price_inc;
       this.count++;
     } else {
-      M.toast({html: "You need more money for this action."})
+      M.toast({html: "You need more money for this action."});
     }
   }
 }
 
 
 let buildings = [
-  new Building("Meeting place", "Recrutes 1 follower per second", 2, function() {
-    followers += this.count * 0.1;
-  }, 1.1)
+  new Building("meeting-place", "Meeting place", "Recruits 1 follower in 10 seconds", 2, function() {
+    followers += this.count * 0.01 * recruiting;
+  }, function() { $("#building-meeting-place").hide(); }, 1.1),
+  new Building("church", "Church", "Recruits 1 follower per second", 100, function() {
+    followers += this.count * 0.1 * recruiting;
+  }, function() { $("#building-church").hide(); }, 1.4),
+  new Building("sacrificial-place", "Sacrificial Place", "Produces $ 0.01 per follower per second", 500, function() {
+    money += money_per_follower * followers / 10;
+  }, function() { $("#building-sacrificial-place").hide(); }, 1.2),
 ];
 
 
 let upgrades = [
+  new Upgrade("unlock-meeting-place", "Unlock Meeting Place", "Unlocks the meeting place", 2, function () {
+    $("#building-meeting-place").show();
+  }),
   new Upgrade("double-fee-0", "Higher fee", "Increases your followers' fee by 100%", 50, function () {
     money_per_follower += money_per_follower;
   }),
-  new Upgrade("unlock-church", "Unlock church", "Unlocks the church building", 100, function () {
-    buildings.push(new Building("Church", "Recrutes 10 followers per second", 100, function() {
-      followers += this.count * 1;
-    }, 1.4));
-    document.getElementById("buildings-card").innerHTML += "" +
-      "<div style='padding-bottom: 10px' class='card col s12'><h6>Church</h6><span>Recrutes 10 followers per second<br>Price: $ <span id='building-" + (buildings.length-1).toString() + "-price'>100</span></span><br>" +
-      "<button onclick='buildings[" + (buildings.length-1).toString() + "].buy()' class='btn-flat orange waves-effect'>Buy</button></div>";
-  })
+  new Upgrade("unlock-church", "Unlock Church", "Unlocks the Church", 100, function () {
+    $("#building-church").show();
+  }),
+  new Upgrade("unlock-sacrificial-place", "Unlock Sacrificial Place", "Unlocks the Sacrificial Place", 500, function() {
+    $("#building-sacrificial-place").show();
+  }),
+  new Upgrade("ancient-relic", "Ancient relic", "Increases recruiting by 50%", 500, function () {
+    recruiting += recruiting / 2;
+  }),
+  new Upgrade("ceremonies", "Ceremonies", "Increases recruiting by 50%", 1500, function () {
+    recruiting += recruiting / 2;
+  }),
+  new Upgrade("sacred-texts", "Sacred Texts", "Increases recruiting by 50%", 3000, function () {
+    recruiting += recruiting / 2;
+  }),
 ];
 
 
@@ -80,39 +100,51 @@ function update_display() {
   $("#follower-stat").text(followers.toFixed(0));
   $("#money-stat").text(money.toFixed(2));
   for(let building in buildings) {
-    $("#building-" + building + "-price").text(buildings[building].price.toFixed(2));
+    $("#building-" + buildings[building].id + "-price").text(buildings[building].price.toFixed(2));
+    $("#building-" + buildings[building].id + "-amount").text(buildings[building].count);
+  }
+  for(let upgrade in upgrades) {
+    if(!upgrades[upgrade].owned) {
+      if(upgrades[upgrade].price <= money) {
+        $("#upgrade-"+upgrades[upgrade].id).removeClass("red").addClass("teal");
+      }
+    }
   }
 }
 
 function tick() {
-  money += followers * (money_per_follower * 0.1);
+  money += followers * money_per_follower * 0.1;
   for (let building in buildings) {
     buildings[building].execute();
   }
 }
 
-document.getElementById("recruit-btn").addEventListener("click", function () {
-  followers += followers_per_click;
-});
-
-
-document.getElementById("buildings-card").innerHTML = '';
-for(let building in buildings) {
-  document.getElementById("buildings-card").innerHTML += "" +
-    "<div style='padding-bottom: 10px' class='card col s12'><h6>" + buildings[building].name + "</h6><span>" +
-    buildings[building].description + "<br/>Price: $ <span id='building-" + building + "-price'>" + buildings[building].price.toFixed(2) + "</span></span><br>" +
-    "<button onclick='buildings[" + building + "].buy()' class='btn-flat orange waves-effect'>Buy</button></div>";
+function init() {
+  // Make the recruit button recruit
+  document.getElementById("recruit-btn").addEventListener("click", function () {
+    followers += followers_per_click;
+  });
+  // Put the buildings into the buildings tab
+  document.getElementById("buildings-card").innerHTML = '';
+  for(let building in buildings) {
+    document.getElementById("buildings-card").innerHTML += "" +
+      "<div id='building-" + buildings[building].id + "' style='padding-bottom: 10px' class='card col s12'><h6>" + buildings[building].name + "</h6><span>" +
+      buildings[building].description + "<br/>Price: $ <span id='building-" + buildings[building].id + "-price'>" + buildings[building].price.toFixed(2) + "</span><br/>Amount: <span id='building-"+buildings[building].id+"-amount'>0</span></span><br>" +
+      "<button onclick='buildings[" + building + "].buy()' class='btn-flat orange waves-effect'>Buy</button></div>";
+    buildings[building].init();
+  }
+  // Put the upgrades into the upgrades tab
+  document.getElementById("upgrades-card").innerHTML = '';
+  for(let upgrade in upgrades) {
+    document.getElementById("upgrades-card").innerHTML += "" +
+      "<div style='padding-bottom: 10px' class='card col s12 red' id='upgrade-"+upgrades[upgrade].id+"'><h6>" + upgrades[upgrade].name + "</h6><span>" +
+      upgrades[upgrade].description + "<span id='upgrade-" + upgrades[upgrade].id + "-price'><br>Price: $ " + upgrades[upgrade].price + "</span></span><br>" +
+      "<button id='upgrade-" + upgrades[upgrade].id + "-btn' onclick='upgrades[" + upgrade + "].buy()' class='btn-flat orange waves-effect'>Buy</button></div>";
+  }
+  const intervalID = window.setInterval(() => {
+    tick();
+    update_display();
+  }, 100);
 }
-document.getElementById("upgrades-card").innerHTML = '';
-for(let upgrade in upgrades) {
-  document.getElementById("upgrades-card").innerHTML += "" +
-    "<div style='padding-bottom: 10px' class='card col s12 red' id='upgrade-"+upgrades[upgrade].id+"'><h6>" + upgrades[upgrade].name + "</h6><span>" +
-    upgrades[upgrade].description + "<span id='upgrade-" + upgrades[upgrade].id + "-price'><br>Price: $ " + upgrades[upgrade].price + "</span></span><br>" +
-    "<button id='upgrade-" + upgrades[upgrade].id + "-btn' onclick='upgrades[" + upgrade + "].buy()' class='btn-flat orange waves-effect'>Buy</button></div>";
-}
 
-const intervalID = window.setInterval(() => {
-  tick();
-  update_display();
-}, 100);
-
+init();
