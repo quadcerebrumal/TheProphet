718cf14ff6f7b5164599cf65da4e98f57542c235
quadcerebrumal <alan.dalisdadavis@gmail.com>
2019-11-13 18:52:17 +0100

    * Added title updater that shows followers and money in the document title

--- js/main.js
+++ js/main.js
@@ -84,6 +84,17 @@ class Building {
 function formatNum(number, fractionDigits) {
   return number.toLocaleString([], {minimumFractionDigits: fractionDigits, maximumFractionDigits: fractionDigits});
 }
+function abbreviateNumber(num, fixed) {
+  if (num === null) { return null; } // terminate early
+  if (num === 0) { return '0'; } // terminate early
+  fixed = (!fixed || fixed < 0) ? 0 : fixed; // number of decimal places to show
+  let b = (num).toPrecision(2).split("e"), // get power
+    k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3), // floor at decimals, ceiling at trillions
+    c = k < 1 ? num.toFixed(0 + fixed) : (num / Math.pow(10, k * 3) ).toFixed(1 + fixed), // divide by power
+    d = c < 0 ? c : Math.abs(c), // enforce -0 is 0
+    e = d + ['', 'K', 'M', 'B', 'T'][k]; // append power
+  return e;
+}
 
 function toggleTheme(){
   // Default to light theme, since that is starting theme
@@ -380,6 +391,9 @@ function init() {
     update_display();
   };
   window.saveIntervalID = window.setInterval(save, 5000);
+  window.titleIntervalID = window.setInterval(() => {
+    document.title = `F ${abbreviateNumber(followers.amount, 0)} | $ ${abbreviateNumber(money.amount, 2)}`;
+  }, 1000);
 }
 
 init();
